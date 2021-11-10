9b125da880221bc5bb0e9d4364776c1846d005f2
cmajor5809 <88701502+cmajor5809@users.noreply.github.com>
2021-11-10 12:39:19 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/JEhB2AlKo/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+    if (error){
+    console.error(error);
+} 
+    else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random * 255) +1;
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        
+        document.getElementById("result_label").innerHTML = 'I can hear - '+
+        results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+
+        (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById('ear.png');
+      
+
+    }
+
+        if (results[0].label == "Meowing") {
+            img.src = 'cat in waffle.jpg';
+        } else if (results[0].label == "Mooing") {
+            img.src = 'cow.jpeg';
+        } else if (results[0].label == "Barking") {
+            img.src = 'pomeranians.jpg';
+        }else if(results[0].label == "Roaring"){
+            img.src = 'Lion.jpg';
+        }
+    }
